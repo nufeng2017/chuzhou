3867f57281972370f3dac92771c807cf4f77b269
xiaoxiang <xiaoxiang@house365.com>
2019-09-30 14:38:35 +0800

    code按钮颜色

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var strs = 's',time,step = 1,isClick = false,timer,timeTotal = 60,str1 = '获取验证码';
 function setintervaltime(){
 	// hui('.hui-get-code').click(function(){
+	hui('.hui-get-code').css({'color':'#d0d0d0','border':'1px solid #d0d0d0'});
 		if (!isClick){
 			isClick = true;
 			time = timeTotal;
@@ -15,6 +16,7 @@ function setintervaltime(){
 			if (time > 0){
 				hui('.hui-get-code').html(time + strs);
 			} else {
+				hui('.hui-get-code').css({'color':'#333'});
 				hui('.hui-get-code').html(str1);
 				isClick = false;
 				clearInterval(timer);
